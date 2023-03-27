5ecd0d5586e4c13c22349f48bfa75881c92b3adc
Chinomasacre <junioryque2014@gmail.com>
2023-03-27 10:34:14 -0500

    agregar en todo las referencias y agregar en el dal

---
+++ DAL/AdministradorRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+
+namespace DAL
+{
+    public class AdministradorRepository
+    {
+        private readonly SqlConnection _connection;
+        public AdministradorRepository(ConnectionManager connection)
+        {
+            _connection = connection._conexion;
+        }
+
+        public Administrador Buscar(string usuario, string contraseña)
+        {
+            SqlDataReader dataReader;
+            using (var command = _connection.CreateCommand())
+            {
+                command.CommandText = "select * from Administrador where Usuario=@Usuario and Contraseña=@Contraseña";
+                command.Parameters.AddWithValue("@Usuario", usuario);
+                command.Parameters.AddWithValue("@Contraseña", contraseña);
+                dataReader = command.ExecuteReader();
+                dataReader.Read();
+                return DataReaderMapToPerson(dataReader);
+            }
+        }
+        private Administrador DataReaderMapToPerson(SqlDataReader dataReader)
+        {
+            if (!dataReader.HasRows) return null;
+            Administrador administrador = new Administrador();
+            administrador.Identificacion = (string)dataReader["Identificacion"];
+            administrador.Usuario = (string)dataReader["Usuario"];
+            administrador.Contraseña = (string)dataReader["Contraseña"];
+            return administrador;
+        }
+    }
+}
